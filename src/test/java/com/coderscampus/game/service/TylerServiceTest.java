6d645c5f5053a1a03a136b7168ae3717c76b62cc
Tyler Miceli <tymiceli24@gmail.com>
2022-11-09 09:37:45 -0500

    issue 11 resolve

--- src/test/java/com/coderscampus/game/service/TylerServiceTest.java
+++ src/test/java/com/coderscampus/game/service/TylerServiceTest.java
@@ -5,14 +5,23 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class TylerServiceTest {
+	
+	TylerService ts = new TylerService();
 
 	@Test
 	void testMultiplyTwoNumbers() {
 		
-		TylerService ts = new TylerService();
 		Integer result = ts.multiplyTwoNumbers(2, 5);
 		
 		assertEquals (result, 10);
 	}
 
+	@Test
+	void testAddTwoNumbers() {
+	
+		Integer result = ts.addTwoNumbers(10,  12);
+		
+		assertEquals (result, 22);
+	
+	}
 }
