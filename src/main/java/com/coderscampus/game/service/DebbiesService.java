66e09ece770bb234c6f4868afc07340073a447a9
Debbie Calton <dcal0223@gmail.com>
2022-12-08 20:45:49 -0500

    commit

--- src/main/java/com/coderscampus/game/service/DebbiesService.java
+++
@@ -1,8 +0,0 @@
-package com.coderscampus.game.service;
-
-public class DebbiesService {
-	public String printHelloWorld() {
-		return "Hello World";
-
-	}
-}
--- src/main/java/com/coderscampus/game/service/example/ExampleMathService.java
+++
@@ -1,8 +0,0 @@
-package com.coderscampus.game.service.example;
-
-public class ExampleMathService {
-    public int multiply (int a, int b) {
-    return a*b;
-}
-
-}
